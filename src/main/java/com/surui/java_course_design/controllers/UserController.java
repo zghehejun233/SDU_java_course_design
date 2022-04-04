7c285c753cfc57968bf5522ab390117b171ca98d
guosurui <13012615613@163.com>
2022-04-05 01:59:10 +0800

    feat: 添加log
    添加log

--- src/main/java/com/surui/java_course_design/controllers/UserController.java
+++ src/main/java/com/surui/java_course_design/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.surui.java_course_design.controllers;
 
 import com.surui.java_course_design.pojo.User;
 import com.surui.java_course_design.service.users.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import javax.annotation.Resource;
 /**
  * @author GuoSurui
  */
+@Slf4j
 @RestController
 @RequestMapping(value = "/api/users")
 public class UserController {
@@ -20,7 +22,9 @@ public class UserController {
 
     @PostMapping(value = "/")
     public String postUser(@RequestBody User user) {
+        log.debug("postUser starts.");
         userService.postUser(user);
+        log.debug("postUser finishes.");
         return "OK";
     }
 }
